ec0e0be18de36b502a0a1945ce464b8f559a109a
Alina <alina_gabur@yahoo.com>
2025-10-25 13:52:21 +0200

    first commit

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,80 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+
+const url = 'https://www.course-api.com/react-useReducer-cart-project';
+
+const initialState = {
+  cartItems: [],
+  amount: 4,
+  total: 0,
+  isLoading: true,
+};
+
+export const getCartItems = createAsyncThunk('cart/getCartItems', async () => {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Failed to fetch cart items")
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+});
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+    increase: (state, action) => {
+      const itemId = action.payload;
+      const cartItem = state.cartItems.find((item) => itemId === item.id);
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, action) => {
+      const itemId = action.payload;
+      const cartItem = state.cartItems.find((item) => item.id === itemId);
+      cartItem.amount = cartItem.amount - 1;
+    },
+    calculateTotals: (state) => {
+      let amount = 0;
+      let total = 0;
+      state.cartItems.forEach((item) => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      });
+      state.amount = amount;
+      state.total = total;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getCartItems.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getCartItems.fulfilled, (state, action) => {
+        // console.log(action);
+        state.isLoading = false;
+        state.cartItems = action.payload;
+      })
+      .addCase(getCartItems.rejected, (state, action) => {
+        // console.log(action);
+        state.isLoading = false;
+      });
+  },
+});
+
+//  console.log(cartSlice)
+export const { clearCart, removeItem, increase, decrease, calculateTotals } =
+  cartSlice.actions;
+
+export default cartSlice.reducer;
